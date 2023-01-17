2f990bd91b549328aa4fb99609c9562c87d4ef12
surajsubrahmanyankellogg <suraj.subrahmanyan@kellogg.northwestern.edu>
2023-01-17 22:00:07 +0000

    Week 3 examples

--- final/school.sql
+++ final/school.sql
@@ -1,10 +1,5 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
-DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS teachers;
-DROP TABLE IF EXISTS courses;
-DROP TABLE IF EXISTS sections;
-DROP TABLE IF EXISTS enrollments;
 
 CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,8 +8,6 @@ CREATE TABLE students (
   email TEXT,
   phone_number TEXT
 );
-
--- Create the rest of the tables
 
 CREATE TABLE teachers (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
